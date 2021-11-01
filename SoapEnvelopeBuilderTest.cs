bfea1f9ce241fc50d7f6ae64800a120f109f6f7f
skvimpact <skvimpact@yandex.ru>
2021-11-01 09:39:44 +0300

    Add project files.

---
+++ SoapEnvelopeBuilderTest.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Diagnostics;
+using Xunit;
+
+namespace Soap.Test
+{
+    public class SoapEnvelopeBuilderTest
+    {
+        [Fact]
+        public void CanCreate()
+        {
+            var envelope = new SoapEnvelopeBuilder()
+                .InCodeunit("SrvLeasingItemMgt")
+                    .CallMethod("UpdateLiquidityLevel")
+                        .WithParameter("updateLiquidityLevelRq", value => Tools.Base64Encode("8hi7ttfjtgbh"))
+                .Envelope();
+                //.Send("http://xxxxxxxxx/Service1.asmx",
+                //      "http://xxxxxxxx/Service1.asmx?op=HelloWorld");
+
+            //envelope.Request();
+//            Console.WriteLine(envelope);
+            Debug.WriteLine(envelope);
+            Debug.WriteLine("That is good");
+            Debug.WriteLine("That is good 2");
+        }
+    }
+}
